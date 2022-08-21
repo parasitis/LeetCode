8a8328d30807ed6a11d9aa9508f3e3855e560104
parasitis <103533252+parasitis@users.noreply.github.com>
2022-08-21 16:27:17 +0530

    Time: 0 ms (100.00%), Space: 54.3 MB (55.55%) - LeetHub

---
+++ 704-binary-search/704-binary-search.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int n=nums.length-1;
+        int l=0;
+        int h=n;
+      //  int mid;
+        
+      // for(int i=0;i<nums.length-1;i++)
+       while(l<=h)
+        {
+            int mid=(l+h)/2;
+          if(nums[mid]==target)
+              return mid;
+            if(target>nums[mid])
+            {
+                l=mid+1;
+            }
+            else 
+                h=mid-1;
+        }
+            return -1;
+    }
+}
