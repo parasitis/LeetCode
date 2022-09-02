4a99cb6f2e6e182116fcdf8e68c07597c445f3d8
parasitis <103533252+parasitis@users.noreply.github.com>
2022-09-02 14:09:14 +0530

    Time: 7 ms (45.18%), Space: 40.8 MB (62.11%) - LeetHub

--- 633-sum-of-square-numbers/633-sum-of-square-numbers.java
+++ 633-sum-of-square-numbers/633-sum-of-square-numbers.java
@@ -1,19 +1,23 @@
 class Solution {
     public boolean judgeSquareSum(int c) {
-        long left=0;
-        long right=(int)Math.sqrt(c);
+       long left=0;
+       long right=(int)Math.sqrt( c);
         while(left<=right)
         {
             if((left*left+right*right)==c)
                 return true;
+            
             else
             {
-            if((left*left+right*right)>c)
-                right=right-1;
-                else
-                    left=left+1;
+            if((left*left+right*right)<c)
+            {
+                left=left+1;
             }
             
+            
+            else
+                right=right-1;
+        }
         }
         return false;
     }
