12de2582a0379be6c6680b881af32df636805e41
parasitis <103533252+parasitis@users.noreply.github.com>
2022-09-03 16:19:36 +0530

    Time: 23 ms (51.57%), Space: 41.3 MB (20.52%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.java
+++ 278-first-bad-version/278-first-bad-version.java
@@ -3,14 +3,17 @@
 
 public class Solution extends VersionControl {
 public int firstBadVersion(int n) {
-int lowerBound = 1;
-int upperBound = n;
-while(lowerBound <= upperBound){
-int mid = lowerBound + (upperBound - lowerBound) / 2;
-boolean ans = isBadVersion(mid);
-if(ans == true) upperBound = mid - 1;
-else lowerBound = mid + 1;
-}
-return lowerBound;
-}
+    int lb=1;
+    int ub=n;
+    while(lb<=ub)
+    {
+        int mid=lb+(ub-lb)/2;
+        boolean ans =  isBadVersion(mid);
+        if(ans==true)
+            ub=mid-1;
+        else
+            lb=mid+1;
+    }
+    return lb;
+    }
 }
