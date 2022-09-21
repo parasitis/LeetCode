57ed0f7e6151ea991284d55c796071fad5aaf051
parasitis <103533252+parasitis@users.noreply.github.com>
2022-09-21 22:20:18 +0530

    Time: 0 ms (100.00%), Space: 39 MB (99.16%) - LeetHub

---
+++ 202-happy-number/202-happy-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isHappy(int n) {
+        int sum=0;
+        while(n>0)
+        {
+            sum+=(n%10)*(n%10);
+            n/=10;
+            if(sum>=9 && n==0)
+            {
+                n=sum;
+                sum=0;
+                
+            }
+        }
+        if(sum==7 || sum==1)
+            return true;
+        else
+            return false;
+    }
+}
