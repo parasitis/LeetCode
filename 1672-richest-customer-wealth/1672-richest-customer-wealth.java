3f47d45d2d9357d2a654ae2e92c186e3bb2d36b9
parasitis <103533252+parasitis@users.noreply.github.com>
2022-09-22 15:51:07 +0530

    Time: 1 ms (61.70%), Space: 43 MB (69.87%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int m=accounts.length;
+        int n=accounts[0].length;
+        int max=0;
+        for(int i=0;i<m;i++)
+        {
+            int sum=0;
+            for(int j=0;j<n;j++)
+            {
+                
+                sum+=accounts[i][j];
+            }
+            max=Math.max(sum,max);
+        }
+        return max;
+    }
+}
